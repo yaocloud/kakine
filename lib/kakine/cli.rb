d0b77377c0a8ca9f941a2a7865cb8ba297c98c73
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-24 12:54:21 +0900

    show detailed rule on security groups

--- lib/kakine/cli.rb
+++ lib/kakine/cli.rb
@@ -13,7 +13,7 @@ module Kakine
 
       security_groups_on_tenant = security_groups.select{|sg| sg.tenant_id == tenant.id}
       security_groups_on_tenant.each do |sg|
-        puts format_security_group(sg)
+        puts format_security_group(sg).to_yaml
       end
     end
 
@@ -23,8 +23,32 @@ module Kakine
       sg_hash = {}
 
       sg_hash[:name] = security_group.name
+      sg_hash[:rule] = []
 
-      sg_hash.to_json
+      security_group.security_group_rules.each do |rule|
+        rule_hash = {}
+
+        rule_hash[:direction] = rule.direction
+        rule_hash[:protocol] = rule.protocol
+
+        if rule.port_range_max == rule.port_range_min
+          rule_hash[:port] = rule.port_range_max
+        else
+          rule_hash[:port_range_max] = rule.port_range_max
+          rule_hash[:port_range_min] = rule.port_range_min
+        end
+
+        if rule.remote_group_id
+          response = Fog::Network[:openstack].get_security_group(rule.remote_group_id)
+          rule_hash[:remote_group] = response.data[:body]["security_group"]["name"]
+        else
+          rule_hash[:remote_ip] = rule.remote_ip_prefix
+        end
+
+        sg_hash[:rule] << rule_hash
+      end
+
+      sg_hash
     end
   end
 end
