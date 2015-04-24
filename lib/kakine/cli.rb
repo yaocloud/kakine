9043eb4b7899bc72382eb173dfd590b92fe1bf2b
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-24 12:29:19 +0900

    added show command

--- lib/kakine/cli.rb
+++ lib/kakine/cli.rb
@@ -1,6 +1,18 @@
 require 'thor'
+require 'fog'
 
 module Kakine
   class CLI < Thor
+    option :tenant, type: :string, aliases: '-t'
+    desc 'show', 'show Security Groups specified tenant'
+    def show
+      security_groups = Fog::Network[:openstack].security_groups
+
+      tenants = Fog::Identity[:openstack].tenants
+      tenant = tenants.detect{|t| t.name == options[:tenant]}
+
+      security_groups_on_tenant = security_groups.select{|sg| sg.tenant_id == tenant.id}
+      pp security_groups_on_tenant
+    end
   end
 end
