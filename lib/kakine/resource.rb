d58fb4a9be73a33e369de4610a155b26f2ed2b93
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-25 15:41:13 +0900

    move security_groups_on_tenant into Resource

--- lib/kakine/resource.rb
+++ lib/kakine/resource.rb
@@ -5,6 +5,11 @@ module Kakine
         tenants = Fog::Identity[:openstack].tenants
         tenants.detect{|t| t.name == tenant_name}
       end
+
+      def security_groups_on_tenant(tenant_name)
+        security_groups = Fog::Network[:openstack].security_groups
+        security_groups.select{|sg| sg.tenant_id == tenant(tenant_name).id}
+      end
     end
   end
 end
