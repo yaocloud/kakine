e282ebd1bcacffc213509cfa58a147527fb31b9b
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-25 15:43:42 +0900

    move Fog access methods to resource

--- lib/kakine/cli.rb
+++ lib/kakine/cli.rb
@@ -8,7 +8,7 @@ module Kakine
     option :tenant, type: :string, aliases: '-t'
     desc 'show', 'show Security Groups specified tenant'
     def show
-      puts security_groups(options[:tenant]).to_yaml
+      puts Kakine::Resource.security_groups_hash(options[:tenant]).to_yaml
     end
 
     option :tenant, type: :string, aliases: "-t"
@@ -18,7 +18,7 @@ module Kakine
     def apply
       adapter = Kakine::Adapter::Mock.new
       filename = options[:filename] ? options[:filename] : "#{options[:tenant]}.yaml"
-      diffs = HashDiff.diff(security_groups(options[:tenant]), YAML.load_file(filename).to_hash)
+      diffs = HashDiff.diff(Kakine::Resource.security_groups_hash(options[:tenant]), YAML.load_file(filename).to_hash)
 
       diffs.each do |diff|
         sg_name, rule_modification = *diff[1].scan(/^([\w-]+)(\[\d\])?/)[0]
@@ -63,47 +63,6 @@ module Kakine
           end
         end
       end
-    end
-
-    private
-
-    def security_groups(tenant_name)
-      sg_hash = {}
-
-      security_groups_on_tenant(tenant_name).each do |sg|
-        sg_hash[sg.name] = format_security_group(sg)
-      end
-
-      sg_hash
-    end
-
-    def format_security_group(security_group)
-      rules = []
-
-      security_group.security_group_rules.each do |rule|
-        rule_hash = {}
-
-        rule_hash["direction"] = rule.direction
-        rule_hash["protocol"] = rule.protocol
-
-        if rule.port_range_max == rule.port_range_min
-          rule_hash["port"] = rule.port_range_max
-        else
-          rule_hash["port_range_max"] = rule.port_range_max
-          rule_hash["port_range_min"] = rule.port_range_min
-        end
-
-        if rule.remote_group_id
-          response = Fog::Network[:openstack].get_security_group(rule.remote_group_id)
-          rule_hash["remote_group"] = response.data[:body]["security_group"]["name"]
-        else
-          rule_hash["remote_ip"] = rule.remote_ip_prefix
-        end
-
-        rules << rule_hash
-      end
-
-      rules
     end
   end
 end
--- lib/kakine/resource.rb
+++ lib/kakine/resource.rb
@@ -10,6 +10,45 @@ module Kakine
         security_groups = Fog::Network[:openstack].security_groups
         security_groups.select{|sg| sg.tenant_id == tenant(tenant_name).id}
       end
+
+      def security_groups_hash(tenant_name)
+        sg_hash = {}
+
+        security_groups_on_tenant(tenant_name).each do |sg|
+          sg_hash[sg.name] = format_security_group(sg)
+        end
+
+        sg_hash
+      end
+
+      def format_security_group(security_group)
+        rules = []
+
+        security_group.security_group_rules.each do |rule|
+          rule_hash = {}
+
+          rule_hash["direction"] = rule.direction
+          rule_hash["protocol"] = rule.protocol
+
+          if rule.port_range_max == rule.port_range_min
+            rule_hash["port"] = rule.port_range_max
+          else
+            rule_hash["port_range_max"] = rule.port_range_max
+            rule_hash["port_range_min"] = rule.port_range_min
+          end
+
+          if rule.remote_group_id
+            response = Fog::Network[:openstack].get_security_group(rule.remote_group_id)
+            rule_hash["remote_group"] = response.data[:body]["security_group"]["name"]
+          else
+            rule_hash["remote_ip"] = rule.remote_ip_prefix
+          end
+
+          rules << rule_hash
+        end
+
+        rules
+      end
     end
   end
 end
