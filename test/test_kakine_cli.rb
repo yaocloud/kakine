3be8e9aad4d3517cbddba5789c3bffb2f7ce44e7
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-27 15:09:40 +0900

    added some tests

--- test/test_kakine_cli.rb
+++ test/test_kakine_cli.rb
@@ -34,11 +34,34 @@ class TestKakineCLI < Minitest::Test
   end
 
   def test_create_security_group_rule
+    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected004.yaml'))
+    Kakine::Resource.stubs(:tenant).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group).returns(Dummy.new)
+
+    Kakine::Adapter::Mock.any_instance.expects(:create_rule).once
+
+    Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
   end
 
   def test_delete_security_group_rule
+    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected005.yaml'))
+    Kakine::Resource.stubs(:security_group).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
+
+    Kakine::Adapter::Mock.any_instance.expects(:delete_rule).once
+
+    Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
   end
 
   def test_update_security_group_rule
+    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected006.yaml'))
+    Kakine::Resource.stubs(:tenant).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
+
+    Kakine::Adapter::Mock.any_instance.expects(:create_rule).once
+    Kakine::Adapter::Mock.any_instance.expects(:delete_rule).once
+
+    Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
   end
 end
