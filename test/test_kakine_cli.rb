0f5c41e3de104818f55394e5e68edf336b7964a8
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-27 12:28:39 +0900

    skelton

---
+++ test/test_kakine_cli.rb
@@ -0,0 +1,21 @@
+require 'minitest_helper'
+
+class TestKakineCLI < Minitest::Test
+  def test_create_security_group
+  end
+
+  def test_create_security_group_with_rule
+  end
+
+  def test_delete_security_group
+  end
+
+  def test_create_security_group_rule
+  end
+
+  def test_delete_security_group_rule
+  end
+
+  def test_update_security_group_rule
+  end
+end
