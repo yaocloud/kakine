271c9f1e5445c22a1c293e32917824caa61081ee
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-04-27 14:27:58 +0900

    added simple mock test

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -5,3 +5,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kakine'
 require 'minitest/autorun'
 require 'mocha/mini_test'
+
+class DummyTenant
+  def id
+    "awesome-id"
+  end
+end
--- test/test_kakine_cli.rb
+++ test/test_kakine_cli.rb
@@ -6,6 +6,12 @@ class TestKakineCLI < Minitest::Test
   end
 
   def test_create_security_group
+    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/expected001.yaml'))
+    Kakine::Resource.stubs(:tenant).returns(DummyTenant.new)
+
+    Kakine::Adapter::Mock.any_instance.expects(:create_security_group).once
+
+    Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
   end
 
   def test_create_security_group_with_rule
