f375801761cbb32e19c3d462f063a43dbd7627ee
kazuhiko yamashita <pyama@pepabo.com>
2015-05-14 13:22:25 +0900

    using modify content format

--- lib/kakine/cli.rb
+++ lib/kakine/cli.rb
@@ -28,6 +28,7 @@ module Kakine
       diffs.each do |diff|
         sg_name, rule_modification = *diff[1].scan(/^([\w-]+)(\[\d\])?/)[0]
 
+        modify_content = Kakine::Resource.format_modify_contents(sg_name, reg_sg, diff)
         if rule_modification # foo[2]
           security_group = Kakine::Resource.security_group(options[:tenant], sg_name)
           if diff[2]["remote_group"]
@@ -35,31 +36,36 @@ module Kakine
             diff[2]["remote_group_id"] = remote_security_group.id
           end
           case diff[0]
+
+          case mod_content["div"]
           when "+"
-            diff[2].merge!({"ethertype" => "IPv4", "tenant_id" => Kakine::Resource.tenant(options[:tenant]).id})
-            adapter.create_rule(security_group.id, diff[2]["direction"], diff[2])
+            mod_content["rules"].merge!({"tenant_id" => Kakine::Resource.tenant(options[:tenant]).id})
+            adapter.create_rule(security_group.id, mod_content["rules"]["direction"], mod_content["rules"])
           when "-"
-            security_group_rule = Kakine::Resource.security_group_rule(security_group, diff[2])
+            security_group_rule = Kakine::Resource.security_group_rule(security_group, mod_content["rules"])
             adapter.delete_rule(security_group_rule.id)
           else
             raise
           end
         else # foo
-          case diff[0]
+          case mod_content["div"]
           when "+"
-            attributes = {name: sg_name, description: "", tenant_id: Kakine::Resource.tenant(options[:tenant]).id}
+            attributes = {name: sg_name, description: mod_content["description"], tenant_id: Kakine::Resource.tenant(options[:tenant]).id}
             security_group_id = adapter.create_security_group(attributes)
-            diff[2].each do |rule|
-              rule.merge!({"ethertype" => "IPv4", "tenant_id" => Kakine::Resource.tenant(options[:tenant]).id})
+            mod_content["rules"].each do |rule|
+              rule.merge!({"tenant_id" => Kakine::Resource.tenant(options[:tenant]).id})
+              adapter.create_rule(security_group_id, rule["direction"], rule)
+            end if mod_content["rules"]
+          when "-"
+            security_group = Kakine::Resource.security_group(options[:tenant], sg_name)
+            adapter.delete_security_group(security_group.id)
+            mod_content["rules"].each do |rule|
               if rule["remote_group"]
                 remote_security_group = Kakine::Resource.security_group(options[:tenant], rule.delete("remote_group"))
                 rule["remote_group_id"] = remote_security_group.id
               end
               adapter.create_rule(security_group_id, rule["direction"], rule)
-            end if diff[2]
-          when "-"
-            security_group = Kakine::Resource.security_group(options[:tenant], sg_name)
-            adapter.delete_security_group(security_group.id)
+            end if mod_content["rules"]
           else
             raise
           end
