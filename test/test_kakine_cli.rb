08067e1aa9a39b2efcdcb44ce6a7f98a8e84bf65
kazuhiko yamashita <pyama@pepabo.com>
2015-05-20 21:43:33 +0900

    add tests

--- test/test_kakine_cli.rb
+++ test/test_kakine_cli.rb
@@ -2,11 +2,11 @@ require 'minitest_helper'
 
 class TestKakineCLI < Minitest::Test
   def setup
-    Kakine::Resource.stubs(:security_groups_hash).returns(YAML.load_file('test/fixtures/cli/actual.yaml'))
+    Kakine::Resource.stubs(:security_groups_hash).returns(Hash[YAML.load_file('test/fixtures/cli/actual.yaml').to_hash.sort].sg_rules_sort)
   end
 
   def test_create_security_group
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected001.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected001.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
@@ -17,7 +17,7 @@ class TestKakineCLI < Minitest::Test
   end
 
   def test_create_security_group_with_rule
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected002.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected002.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
@@ -29,7 +29,7 @@ class TestKakineCLI < Minitest::Test
   end
 
   def test_delete_security_group
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected003.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected003.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
 
@@ -39,18 +39,19 @@ class TestKakineCLI < Minitest::Test
   end
 
   def test_create_security_group_rule
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected004.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected004.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
 
-    Kakine::Adapter::Mock.any_instance.expects(:create_rule).once
+    Kakine::Adapter::Mock.any_instance.expects(:delete_rule).times(2)
+    Kakine::Adapter::Mock.any_instance.expects(:create_rule).times(3)
 
     Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
   end
 
   def test_delete_security_group_rule
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected005.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected005.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
@@ -61,19 +62,45 @@ class TestKakineCLI < Minitest::Test
   end
 
   def test_update_security_group_rule
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected006.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected006.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
 
-    Kakine::Adapter::Mock.any_instance.expects(:create_rule).once
-    Kakine::Adapter::Mock.any_instance.expects(:delete_rule).once
+    Kakine::Adapter::Mock.any_instance.expects(:create_rule).twice
+    Kakine::Adapter::Mock.any_instance.expects(:delete_rule).twice
 
     Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
   end
 
   def test_update_security_group_description
-    Kakine::Resource.stubs(:yaml).returns(YAML.load_file('test/fixtures/cli/expected007.yaml'))
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected007.yaml').to_hash.sort].sg_rules_sort)
+    Kakine::Resource.stubs(:tenant).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
+
+    Kakine::Adapter::Mock.any_instance.expects(:delete_security_group).once
+    Kakine::Adapter::Mock.any_instance.expects(:create_security_group).once
+
+    Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
+  end
+
+  def test_change_rule_position
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected008.yaml').to_hash.sort].sg_rules_sort)
+    Kakine::Resource.stubs(:tenant).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group).returns(Dummy.new)
+    Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
+
+    Kakine::Adapter::Mock.any_instance.expects(:delete_security_group).never
+    Kakine::Adapter::Mock.any_instance.expects(:create_security_group).never
+    Kakine::Adapter::Mock.any_instance.expects(:create_rule).never
+    Kakine::Adapter::Mock.any_instance.expects(:delete_rule).never
+
+    Kakine::CLI.new.invoke(:apply, [], {dryrun: true})
+  end
+
+  def test_no_rule_group
+    Kakine::Resource.stubs(:yaml).returns(Hash[YAML.load_file('test/fixtures/cli/expected009.yaml').to_hash.sort].sg_rules_sort)
     Kakine::Resource.stubs(:tenant).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group).returns(Dummy.new)
     Kakine::Resource.stubs(:security_group_rule).returns(Dummy.new)
