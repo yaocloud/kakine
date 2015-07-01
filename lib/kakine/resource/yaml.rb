e11485971b13fbfe9ebdf79d8e42619ca20126a7
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-02 01:15:51 +0900

    いらない例外処理を削除

--- lib/kakine/resource/yaml.rb
+++ lib/kakine/resource/yaml.rb
@@ -5,11 +5,7 @@ module Kakine
         def load_security_group(filename, tenant_name)
           load_yaml = yaml(filename)
           Kakine::Validate.validate_file_input(load_yaml)
-
           load_yaml.map { |sg| Kakine::SecurityGroup.new(tenant_name, sg) }
-
-          raise(Kakine::Errors::Configure, "can't load config by yaml") unless load_yaml
-          load_yaml
         end
 
         def yaml(filename)
