fc42b7255e8e19086c186ad17ee3a367a5d34a5a
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-02 00:11:47 +0900

    例外クラス追加

--- lib/kakine.rb
+++ lib/kakine.rb
@@ -6,6 +6,7 @@ require 'kakine/resource'
 require 'kakine/security_group'
 require 'kakine/security_rule'
 require 'kakine/validate'
+require 'kakine/errors'
 
 module Kakine
 end
---
+++ lib/kakine/errors.rb
@@ -0,0 +1,6 @@
+module Kakine
+  module Errors
+    class Error < StandardError; end
+    class Configure < Error; end
+  end
+end
