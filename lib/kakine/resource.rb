5b0959a6dea7429b793a361bac4499c920f24c0a
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-02 00:42:09 +0900

    format

--- lib/kakine/resource.rb
+++ lib/kakine/resource.rb
@@ -5,8 +5,10 @@ module Kakine
     class << self
       def load_security_group_by_yaml(filename, tenant_name)
         load_yaml = yaml(filename)
+
         Kakine::Validate.validate_file_input(load_yaml)
         load_yaml.map { |sg| Kakine::SecurityGroup.new(tenant_name, sg) }
+
         raise(Kakine::Errors::Configure, "can't load config by yaml") unless load_yaml
         load_yaml
       end
