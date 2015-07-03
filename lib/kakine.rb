2a5b488d276fce64807ebe12523efb16dc6181ec
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-04 03:15:32 +0900

    validateはyamlのみで使用されているので分割しない

--- lib/kakine.rb
+++ lib/kakine.rb
@@ -11,7 +11,6 @@ require 'kakine/resource/openstack'
 require 'kakine/resource/yaml'
 require 'kakine/security_group'
 require 'kakine/security_rule'
-require 'kakine/validate'
 require 'kakine/errors'
 
 module Kakine
