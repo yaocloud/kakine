c3c172d8d3e3a9d049ee65d0b5c596d0af7f2fa0
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-04 02:12:51 +0900

    ポートフォーマット変更の例外を追加

--- lib/kakine/errors.rb
+++ lib/kakine/errors.rb
@@ -2,5 +2,6 @@ module Kakine
   module Errors
     class Error < StandardError; end
     class Configure < Error; end
+    class SecurityRule < Error; end
   end
 end
--- lib/kakine/security_rule.rb
+++ lib/kakine/security_rule.rb
@@ -27,22 +27,28 @@ module Kakine
     private
 
     def convert_port_format(rule)
-      case
-      when rule.has_key?('port')
-        [rule['port'] ,rule['port']]
-      when rule.has_key?('type'), rule.has_key?('code')
-        [rule['type'] ,rule['code']]
-      when rule.has_key?('port_range_max'), rule.has_key?('port_range_min')
-        [rule['port_range_min'] ,rule['port_range_max']]
-      else
-        raise "no match port format"
+      unless format = port?(rule) || icmp?(rule) || range?(rule)
+        raise(Kakine::Errors::SecurityRule, "no match port format")
       end
+      format
+    end
+
+    def port?(rule)
+      Array.new(rule['port'] ,rule['port']) if rule.has_key?('port')
+    end
+
+    def icmp?(rule)
+      Array.new(rule['type'] ,rule['code']) if rule.has_key?('type') && rule.has_key?('code')
+    end
+
+    def range?(rule)
+      Array.new(rule['port_range_min'] ,rule['port_range_max']) if rule.has_key?('port_range_max') && rule.has_key?('port_range_min')
     end
 
     def set_remote_security_group_id
       unless @remote_group.nil?
         remote_security_group = Kakine::Resource.get(:openstack).security_group(@tenant_name, @remote_group)
-        raise "not exists #{@remote_group}" unless remote_security_group
+        raise(Kakine::Errors::SecurityRule, "not exists #{@remote_group}") unless remote_security_group
         @remote_group_id = remote_security_group.id
       end
     end
