5bd72852da0210b27632c3e80c858014b98eeae7
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-08 01:00:01 +0900

    必須ルールのテスト

--- test/test_kakine_config_validate.rb
+++ test/test_kakine_config_validate.rb
@@ -18,4 +18,34 @@ class TestKakineConfigValidate < Minitest::Test
       Kakine::Resource.get(:yaml).validate_attributes(Kakine::ValidateTest::Helper.lost_description)
     end
   end
+  
+  def test_validate_rules
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_port)
+    end
+
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_port_min)
+    end
+
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_port_code)
+    end
+
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_remote)
+    end
+    
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_direction)
+    end
+    
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_protocol)
+    end
+    
+    assert_raises Kakine::Errors::Configure do
+      Kakine::Resource.get(:yaml).validate_rules(Kakine::ValidateTest::Helper.lost_ethertype)
+    end
+  end
 end
