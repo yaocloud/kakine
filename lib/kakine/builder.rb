0bd60d547bd89e029f26575071dfeed4311fbe6e
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-12 00:44:55 +0900

    メソッド順序の入れ替え

--- lib/kakine/builder.rb
+++ lib/kakine/builder.rb
@@ -12,26 +12,6 @@ module Kakine
         Kakine::Adapter.instance.delete_security_group(sg.id)
       end
 
-      def create_security_rule(tenant_name, sg_name, rule)
-        sg = Kakine::Resource.get(:openstack).security_group(tenant_name, sg_name)
-        security_group_id =  Kakine::Options.dryrun? && sg.nil? ? Kakine::Adapter.instance.id(sg_name) : sg.id
-        Kakine::Adapter.instance.create_rule(security_group_id, rule.direction, rule)
-      end
-
-      def delete_security_rule(tenant_name, sg_name, rule)
-        Kakine::Adapter.instance.delete_rule(rule.id)
-      end
-
-      def delete_default_security_rule(tenant_name, sg_name)
-        target_sg = Kakine::Resource.get(:openstack).load_security_group.find do |sg|
-          sg.name == sg_name
-        end
-
-        target_sg.rules.map do |rule|
-          delete_security_rule(tenant_name, sg_name, rule)
-        end if target_sg
-      end
-
       def create_new_security_group(new_sg)
         create_security_group(new_sg)
         new_sg.rules.map do |rule|
@@ -59,8 +39,32 @@ module Kakine
         create_new_security_group(new_sg)
       end
       
+      def already_setup_security_group(new_sg, current_sgs)
+        current_sgs.find { |current_sg| current_sg.name == new_sg.name }
+      end
+      
+      def create_security_rule(tenant_name, sg_name, rule)
+        sg = Kakine::Resource.get(:openstack).security_group(tenant_name, sg_name)
+        security_group_id =  Kakine::Options.dryrun? && sg.nil? ? Kakine::Adapter.instance.id(sg_name) : sg.id
+        Kakine::Adapter.instance.create_rule(security_group_id, rule.direction, rule)
+      end
+
+      def delete_security_rule(tenant_name, sg_name, rule)
+        Kakine::Adapter.instance.delete_rule(rule.id)
+      end
+
+      def delete_default_security_rule(tenant_name, sg_name)
+        target_sg = Kakine::Resource.get(:openstack).load_security_group.find do |sg|
+          sg.name == sg_name
+        end
+
+        target_sg.rules.map do |rule|
+          delete_security_rule(tenant_name, sg_name, rule)
+        end if target_sg
+      end
+      
       def create_new_rule(new_sg, current_sg)
-        new_sg.rules.each do |rule|
+        new_sg.rules.map do |rule|
           unless current_sg.find_by_rule(rule)
             create_security_rule(new_sg.tenant_name, new_sg.name, rule)
           end
@@ -71,10 +75,6 @@ module Kakine
         current_sg.rules.map do |rule|
           delete_security_rule(new_sg.tenant_name, new_sg.name, rule) unless new_sg.find_by_rule(rule)
         end
-      end
-      
-      def already_setup_security_group(new_sg, current_sgs)
-        current_sgs.find { |current_sg| current_sg.name == new_sg.name }
       end
 
       def show_security_groups
