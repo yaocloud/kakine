cbda89e96458ff1e62087e8e9686c952591f5cd6
kazuhiko yamahsita <pyama@pepabo.com>
2015-07-18 11:12:22 +0900

    メソッド名の見直し https://github.com/hsbt/kakine/pull/8#discussion_r34832529

--- lib/kakine/adapter/real.rb
+++ lib/kakine/adapter/real.rb
@@ -4,7 +4,7 @@ module Kakine
       include Kakine::Adapter::Base
       def create_rule(security_group_id, direction, security_rule)
         begin
-          Fog::Network[:openstack].create_security_group_rule(security_group_id, direction, get_rule_attributes(security_rule))
+          Fog::Network[:openstack].create_security_group_rule(security_group_id, direction, symbolized_rule(security_rule))
         rescue Excon::Errors::Conflict, Excon::Errors::BadRequest => e
           error_message(e.response[:body])
         rescue Kakine::Errors::SecurityRule => e
@@ -18,7 +18,7 @@ module Kakine
 
       def create_security_group(attributes)
         begin
-          response = Fog::Network[:openstack].create_security_group(get_group_attributes(attributes))
+          response = Fog::Network[:openstack].create_security_group(symbolized_group(attributes))
           response.data[:body]["security_group"]["id"]
         rescue Excon::Errors::Conflict, Excon::Errors::BadRequest => e
           error_message(e.response[:body])
@@ -43,11 +43,11 @@ module Kakine
         end
       end
 
-      def get_group_attributes(attributes)
+      def symbolized_group(attributes)
         attributes.inject({}){|data,(k,v)|data[k.to_sym] = v; data }
       end
 
-      def get_rule_attributes(security_rule)
+      def symbolized_rule(security_rule)
         attributes = {}
         %w(protocol port_range_max port_range_min remote_ip ethertype).each do |k|
           attributes[k.to_sym] = security_rule.send(k)
