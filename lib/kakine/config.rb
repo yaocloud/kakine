4527589474612cf8d02914905c3bab76471d53ad
r_takaishi <ryo.takaishi.0@gmail.com>
2018-12-26 14:13:55 +0900

    support keystone v3 api

--- lib/kakine/config.rb
+++ lib/kakine/config.rb
@@ -3,7 +3,7 @@ require 'yaml'
 
 module Kakine
   class Config
-    OS_PARAMS = %w[auth_url username password]
+    OS_PARAMS = %w[auth_url username password identity_api_version user_domain_name project_domain_name]
 
     @@config = {}
 
@@ -51,6 +51,9 @@ module Kakine
         client_cert @@config['client_cert']
         client_key  @@config['client_key']
         region_name @@config['region_name']
+        identity_api_version @@config['identity_api_version']
+        user_domain_name     @@config['user_domain_name']
+        project_domain_name  @@config['project_domain_name']
       end
     end
   end
